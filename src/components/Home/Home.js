aeed6a212d85329ce134702f2a8d9c97c75e7f02
kdoxsey <kdoxse@gmail.com>
2020-12-18 13:32:53 -0500

    Create curl script works

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,7 +16,9 @@ export default function Home () {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <WrappedMap
-        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
+          libraries=geometry,drawing,places&key=
+          ${process.env.REACT_APP_GOOGLE_KEY}`}
         loadingElement={<div style={{ height: '100%' }} />}
         containerElement={<div style={{ height: '100%' }} />}
         mapElement={<div style={{ height: '100%' }} />}
