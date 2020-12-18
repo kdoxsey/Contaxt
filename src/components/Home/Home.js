c3e8e97435b0d8cb4e9801e0906c392cdb89ff74
kdoxsey <kdoxse@gmail.com>
2020-12-18 02:17:56 -0500

    Map is displayed on sign up

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
 
-const Home = () => (
-  <div>
-    map will go here
-  </div>
-)
+function Map () {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 44.427963, lng: -110.588455 }}
+    />
+  )
+}
 
-export default Home
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+export default function Home () {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <WrappedMap
+        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
+    </div>
+  )
+}
