6f1ab9f1eec8580e260607993a3b7e95362b731e
kdoxsey <kdoxse@gmail.com>
2020-12-21 14:24:48 -0500

    Contacts index and '+' button added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import ContactIndex from './components/Contact/ContactIndex'
 import ContactMap from './../src/components/Contact/ContactMap'
+import ContactCreate from './../src/components/Contact/ContactCreate'
 import Header from './../src/components/Header/Header'
 
 function App () {
   return <div>
     <Header />
     <Link to="/contacts">Contacts</Link>
+    <Link to="/contact-create">+</Link>
     <Route path="/contacts" component={ContactIndex}/>
+    <Route path="/contact-create" component={ContactCreate}/>
     <ContactMap />
   </div>
 }
--- src/components/Contact/ContactIndex.js
+++ src/components/Contact/ContactIndex.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const ContactIndex = () => {
-  return (
-    <div>
-      <h2>list of contacts</h2>
-      <ul>
-        <li>contact 1</li>
-        <li>contact 2</li>
-        <li>contact 3</li>
-      </ul>
-    </div>
-  )
+class ContactIndex extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      contacts: null
+    }
+  }
+  render () {
+    return (
+      <div>
+        <h2>contacts</h2>
+        <ul>
+          <li>contact 1</li>
+          <li>contact 2</li>
+          <li>contact 3</li>
+        </ul>
+      </div>
+    )
+  }
 }
+// <div>
+//   <h2>list of contacts</h2>
+//   <ul>
+//     <li>contact 1</li>
+//     <li>contact 2</li>
+//     <li>contact 3</li>
+//   </ul>
+// </div>
 
 export default ContactIndex
