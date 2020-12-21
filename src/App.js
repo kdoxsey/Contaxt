27b9b1e241e3df725274c8c4269f9a091cb941b0
kdoxsey <kdoxse@gmail.com>
2020-12-21 10:46:39 -0500

    Removed messy contact components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@
 import React from 'react'
 import ContactMap from './../src/components/Contact/ContactMap'
 import Header from './../src/components/Header/Header'
-import ContactCreate from './../src/components/Contact/ContactCreate'
 
 function App () {
   return <div>
     <Header />
-    <ContactCreate />
     <ContactMap />
   </div>
 }
