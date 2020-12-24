c4e602496bd4a334036762c98b2d2b79a0ceaf14
kdoxsey <kdoxse@gmail.com>
2020-12-23 23:49:26 -0500

    SHOW now distinguishes between person and Business
    I used an if statement in the Contact Card that checks
    to see if the first and last name fields exist.
    If they do, load lastName, firstName.
    If they don't, load businessName.

--- src/components/Card/ContactCard.js
+++ src/components/Card/ContactCard.js
@@ -3,23 +3,39 @@ import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const ContactCard = ({ item }) => {
-  console.log(item._id)
-  console.log(item.firstName)
-  console.log(item.latitude)
-  return (
-    <Card>
-      <Card.Body>
-        <Card.Title><Link to ={`/contacts/${item._id}`}>
-          {item.lastName}, {item.firstName}</Link>
-        </ Card.Title>
-        <Card.Text>
-          {item.streetAddress}<br />
-          {item.phoneNumber}<br />
-          {item.emailAddress}<br />
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  )
+  if (item.firstName && item.lastName) {
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title><Link to ={`/contacts/${item._id}`}>
+            {item.lastName}, {item.firstName}</Link>
+          </ Card.Title>
+          <Card.Text>
+            {item.streetAddress}<br />
+            {item.phoneNumber}<br />
+            {item.emailAddress}<br />
+            {item.note}<br />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    )
+  } else {
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title><Link to ={`/contacts/${item._id}`}>
+            {item.organizationName}</Link>
+          </ Card.Title>
+          <Card.Text>
+            {item.streetAddress}<br />
+            {item.phoneNumber}<br />
+            {item.emailAddress}<br />
+            {item.note}<br />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    )
+  }
 }
 
 export default ContactCard
