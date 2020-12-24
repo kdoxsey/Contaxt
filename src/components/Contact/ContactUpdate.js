96558f1612eeb5d247fbcc33567fc79004346fed
kdoxsey <kdoxse@gmail.com>
2020-12-23 23:49:26 -0500

    SHOW now distinguishes between person and Business
    I used an if statement in the Contact Card that checks
    to see if the first and last name fields exist.
    If they do, load lastName, firstName.
    If they don't, load businessName.

--- src/components/Contact/ContactUpdate.js
+++ src/components/Contact/ContactUpdate.js
@@ -16,20 +16,67 @@ const ContactUpdate = props => {
     longitude: '',
     note: ''
   })
-  const [updatedContact, seUpdatedContact] = useState(false)
+  const [updatedContact, setUpdatedContact] = useState(false)
 
   const { user, msgAlert, match } = props
   useEffect(() => {
     showContact(user, match.params.contactId)
+      .then(res => setContact(res.data.contact))
       .then(() => msgAlert({
         heading: 'Contact show success',
+        message: 'check it out',
+        variant: 'success'
+      }))
+      .catch(err => msgAlert({
+        heading: 'Task show failed',
         message: err.message,
         variant: 'danger'
       }))
   }, [])
 
   const handleChange = event => {
-    event.persist
-    console.log(event)
+    event.persist()
+    console.log('handleChange event is ' + event)
+
+    // setContact(oldContact => {
+    //   let updatedField
+    //   if (event.target.)
+    // })
   }
+
+  const handleSubmit = event => {
+    console.log('handleSubmit event is ' + event)
+    event.preventDefault()
+    updateContact(user, contact, match.params.contactId)
+      .then(() => setUpdatedContact(true))
+      .then(() => msgAlert({
+        heading: 'Update succesful',
+        message: 'contact updated',
+        variant: 'success'
+      }))
+      .catch(err => msgAlert({
+        heading: 'Update failed',
+        message: err.message,
+        variant: 'danger'
+      }))
+  }
+
+  if (updatedContact) {
+    return <Redirect to={`/contacts/${match.params.id}`} />
+  }
+  handleChange()
+  handleSubmit()
+  console.log([updatedContact, setUpdatedContact])
+  console.log(contact)
+  return (
+    <ContactForm
+    // contact={contact}
+    // handleChange={handleChange}
+    // handleSubmit={handleSubmit}
+    // match={match}
+    // user={user}
+    />
+  )
 }
+
+export default ContactUpdate
