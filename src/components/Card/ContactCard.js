94fe03e8cf7a70dd39994324f6dc4ce7a613ca5c
kdoxsey <kdoxse@gmail.com>
2020-12-27 16:45:55 -0500

    Main branch up to date

--- src/components/Card/ContactCard.js
+++ src/components/Card/ContactCard.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const ContactCard = ({ item }) => {
-  if ((item.firstName || item.lastName) && (item.organizationName)) {
+  if (item.firstName || item.lastName) {
     return (
       <Card
         className="contactCard">
