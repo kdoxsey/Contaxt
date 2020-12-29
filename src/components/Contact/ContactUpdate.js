72b1f60869d7f8598f113ff08cc1c851449957ae
kdoxsey <kdoxse@gmail.com>
2020-12-29 15:36:31 -0500

    Solved "delete not instantly showing on page" bug

--- src/components/Contact/ContactUpdate.js
+++ src/components/Contact/ContactUpdate.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ContactForm from '../Contact/ContactForm'
 import { Redirect } from 'react-router-dom'
-// import ContactIndex from './ContactIndex'
 
 import { updateContact, showContact } from '../../api/contact'
 
