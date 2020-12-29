f08b66c2e7c2e4926980d29a37b43d0c7fc7907e
kdoxsey <kdoxse@gmail.com>
2020-12-29 16:00:22 -0500

    Commented out custom Marker
    Unable to load on deploy

--- src/components/Contact/ContactMap.js
+++ src/components/Contact/ContactMap.js
@@ -24,10 +24,10 @@ function Map (props) {
           onClick={() => {
             setSelectedContact(contact)
           }}
-          icon={{
-            url: '/contaxt_marker.png',
-            scaledSize: new window.google.maps.Size(30, 40)
-          }}
+          // icon={{
+          //   url: '/contaxt_marker.png',
+          //   scaledSize: new window.google.maps.Size(30, 40)
+          // }}
         />
       ))}
 
